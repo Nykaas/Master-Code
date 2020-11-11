cd8446565876213b7d7fdf8e23a22d2f4ec9651a
Ruben <rubennykaas@gmail.com>
2020-11-11 15:15:59 +0100

    Update polarization plotting

--- insitu.py
+++ insitu.py
@@ -6,19 +6,27 @@ import os
 from plot import plot_settings
 
 def in_situ_plot(df, excelfile):
-    print('Doing insitu stuff!')
+    colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink']
     for sheet in df: # Iterate sheet name as key in df dictionary
-        print(sheet)
         columns = list(df[sheet].columns)
+        switch = True
+        color_index = 0
         for i in range(1, len(columns), 3): # Iterate data columns
             xdata = np.array(df[sheet][columns[i]].tolist())
             ydata = np.array(df[sheet][columns[i+1]].tolist())
             name = columns[i+2]
             xlabel = df[sheet]['Graph_settings'][1]
             ylabel = df[sheet]['Graph_settings'][2]
-            
+             
             ### Sheet plotting ###
-            #if sheet == 'Polarization' or sheet == 'EIS' or sheet == 'Durability': # Plot without further calculation
-            plt.plot(xdata, ydata, label = name)
-
+            if sheet == 'Polarization':
+                if switch:
+                    plt.plot(xdata, ydata, linestyle = '-', color = colors[color_index], label = name)
+                    switch = False
+                else:
+                    plt.plot(xdata, ydata, linestyle = '--', color = colors[color_index], label = name)
+                    switch = True
+                    color_index += 1
+            else:
+                plt.plot(xdata, ydata, label = name)
         plot_settings(xlabel, ylabel, columns, sheet, excelfile)
