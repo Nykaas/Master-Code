eba50075a319635eb0f45b09ff882fbc2b762edb
chidonnoli <chido_lfc@hotmail.com>
2020-11-13 13:23:48 +0100

    Updated plot settings
    Correct labels with math

--- plot.py
+++ plot.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 def plot_settings(xlabel, ylabel, title, columns, sheet, excelfile):
     if len(columns) > 3:
         plt.legend()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    #plt.title(title)
+    plt.xlabel(xlabel) # Include fontweight='bold' to bold the label
+    plt.ylabel(ylabel) # Include fontweight='bold' to bold the label
     username = os.getlogin()
     filepath = os.path.join(r'C:\Users', username, r'OneDrive\Specialization Project\3_Project plan\Lab\Plots\Draft', username, excelfile[:-5], sheet) # for data in onedrive
     plt.savefig(filepath, dpi = 300, bbox_inches='tight')
