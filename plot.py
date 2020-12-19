8d356d2eadf1fd4bc3573d3865a010eb1cf40809
Ruben <rubennykaas@gmail.com>
2020-12-19 15:03:13 +0100

    Change to 12 size fonts

--- plot.py
+++ plot.py
@@ -2,12 +2,13 @@ import os
 import matplotlib.pyplot as plt
 
 def plot_settings(xlabel, ylabel, columns, sheet, excelfile):
-    plt.xlabel(xlabel) # Include fontweight='bold' to bold the label
-    plt.ylabel(ylabel) # Include fontweight='bold' to bold the label
+    plt.xlabel(xlabel, fontsize = 12) # Include fontweight='bold' to bold the label
+    plt.ylabel(ylabel, fontsize = 12) # Include fontweight='bold' to bold the label
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    #plt.xlim(-5,95)
     if len(columns) > 3:
-        plt.legend()
-    if sheet == 'OptimalParameters':
-        plt.legend(loc = 'upper center', ncol = 3, bbox_to_anchor=(0.5, 1.22))
+        plt.legend(fontsize = 12)
     username = os.getlogin()
     filepath = os.path.join(r'C:\Users', username, r'OneDrive\Specialization Project\3_Project plan\Lab\Plots\Draft', username, excelfile[:-5], sheet) # for data in onedrive
     plt.savefig(filepath, dpi = 300, bbox_inches='tight')
