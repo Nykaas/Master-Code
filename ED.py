4db2f2101d8e183f9cf23cc88bd21dd25a46fc59
Nykaas <rubennykaas@gmail.com>
2021-04-09 14:34:49 +0200

    Update ED.py

--- ED.py
+++ ED.py
@@ -30,7 +30,7 @@ def ED_plot(df, excelfile, writer, smooth, markers):
 
             if '-' in name and 'V' in name: # Correct Ag/Cl offset in label
                 idx = name.find('V')
-                E = round(float(name[idx-5:idx-1]) + offset_AgCl, 2)
+                E = round(float(name[idx-5:idx-1]) - offset_AgCl, 2) # - offset since float is positive from excel
                 name = name.replace(name[idx-5:idx-1], str(E))
                 name += ' RHE'
                 print(f'Label: AgCl offset {name}')
@@ -74,7 +74,11 @@ def ED_plot(df, excelfile, writer, smooth, markers):
             if 'CV' in sheet: # CV
                 xlabel = r'E [V vs. RHE]'
                 ylabel = r'Current density [mA $\mathdefault{cm^{-2}}$]'
-                plt.plot(x + offset_AgCl, y/A_sample, label = name, marker = markers[markers_idx], markevery = 0.1)
+                plt.plot(x + offset_AgCl, y/A_sample, label = name, marker = markers[markers_idx], markevery = 100, markersize = get_markersize())
+                
+                if 'Electrolytes' in sheet:
+                    plt.xlim(right=-0.4)
+                    save_Eeq_data(x, y, writer, name, offset_AgCl, Eeq_data, sheet, A_sample)
 
             elif 'CP' in sheet: # Constant potential
                 xlabel = r'Time [s]'
@@ -89,12 +93,19 @@ def ED_plot(df, excelfile, writer, smooth, markers):
             markers_idx += 1
         plot_settings(xlabel, ylabel, columns, sheet, excelfile, ECSA_norm=False)
 
-def save_CE_data(m_t, m_a, CE, loading, CE_data, writer, name, I, t):
-    CE_temp = {'Sample': name.replace(r'A $\mathdefault{cm^{-2}}$', 'A cm-2'), 'Current [A]': I, 'Time [s]': t, 'm_t [g]':round(m_t,2), 'm_a [g]':round(m_a,2), 'CE [%]':round(CE,2), 'Loading [mg/cm2]':round(loading,2)}
-    CE_data.append(CE_temp)
-    CE_df = pd.DataFrame(CE_data, columns = ['Sample', 'Current [A]', 'Time [s]', 'm_t [g]', 'm_a [g]', 'CE [%]', 'Loading [mg/cm2]'])
-    CE_df.to_excel(writer, index = False, header=True, sheet_name='CE')
-    writer.save()
+def save_Eeq_data(x, y, writer, name, offset_AgCl, Eeq_data, sheet, A_sample):
+    y_ = y
+    for i, current in enumerate(y):
+            if current < -0.2:
+                Ieq = current
+                Eeq = x[i] + offset_AgCl
+                temp = {'Sheet': sheet, 'Sample': name, 'idx': i, 'Eeq [V, RHE]': Eeq, 'Ieq [mA]': Ieq, 'Ip [mA/cm^2]': y_[-1]/A_sample}
+                Eeq_data.append(temp)
+                Eeq_df = pd.DataFrame(Eeq_data, columns = ['Sheet', 'Sample', 'idx', 'Eeq [V, RHE]', 'Ieq [mA]', 'Ip [mA/cm^2]'])
+                Eeq_df.to_excel(writer, index = False, header=True, sheet_name='Eeq')
+                writer.save()
+                #print(sheet, name, i, Ieq, Eeq)
+                break
 
 def get_ECSA(df):
     columns = list(df['ECSA-cap'].columns)
