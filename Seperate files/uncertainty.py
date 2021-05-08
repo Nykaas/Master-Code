63c00322d35e3253380a7dbe73f6d3a77fbb9ca1
Ruben <rubennykaas@gmail.com>
2021-05-08 18:52:31 +0200

    Uncertainty values

--- Seperate files/uncertainty.py
+++ Seperate files/uncertainty.py
@@ -1,18 +1,19 @@
 import statistics
+
 uncertainty = {
-    'NF_Overpotential': [381, 393, 387],
-    'NF_ECSA': [609.85, 515.91, 617.88],
-    #'NF_bzt': [],
+    'NF_Overpotential': [381, 393, 387], # Ex_Reference
+    'NF_ECSA': [440.61, 316.73, 376.97], # Ex_Reference
+    'NF_bzt': [21.86, 27.3, 24.57], # Ex_Reference
     'ED_loading': [1.98, 1.66, 1.22],
-    'ED_Overpotential': [308, 305, 331],
-    'ED_ECSA': [3756.97, 1219.24, 609.7],
-    #'ED_bzt': []
+    'ED_Overpotential': [308, 305, 331], # Ex_Uncertainty
+    'ED_ECSA': [460.15, 715.91, 370.61], # Ex_Uncertainty
+    'ED_bzt': [14.72, 16.27, 17.41] # Ex_Uncertainty
 }
 
 for key in uncertainty:
     sd = statistics.stdev(uncertainty[key])
     mean = sum(uncertainty[key])/len(uncertainty[key])
-    sd_percent = sd/(mean) * 100
+    sd_percent = sd/mean * 100
     print(f'SD {key} = {round(sd, 2)} or {round(sd_percent, 2)} %')
 
 
